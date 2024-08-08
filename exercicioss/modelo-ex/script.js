dfb736cafd7328d1469c689348e379c1d0e3888d
jefersonstrindade <jeferson.trindade1995@gmail.com>
2024-08-08 16:28:52 -0300

    ok

--- exercicioss/modelo-ex/script.js
+++ exercicioss/modelo-ex/script.js
@@ -1,15 +1,20 @@
-function {
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+function contar(){
+    let inicio = document.getElementById('iinicio')
+    let fim = document.getElementById('ifim')
+    let passo = document.getElementById('ipasso')
+    let res = document.getElementById('res')
 
-    if (hora >= 0 && hora < 12){
-        img.src = 'manha.jpg'
-    } else if (hora >= 12 && hora < 18){
-        img.src = 'tarde.jpg'
-    } else {
-        img.src = 'noite.jpg'
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else{
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c = i; c <=f; c += p) {
+            res.innerHTML += `${c} `
+        }
     }
-}
+}   
